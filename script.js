e0aad4c9d9505093b58cd702a40cda14bbecd2e7
21lama <lamasabaneh671@gmail.com>
2025-06-16 16:44:18 +0300

    add tabs js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
  const input = document.getElementById('newTodo');
  const inputError = document.getElementById('inputError');
  const taskListEl = document.getElementById('taskList');
+ const tabs = document.querySelectorAll('.tabs button');
