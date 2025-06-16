2703fdca0cba2fb3c6862c56bf54cb6ed0ae5e08
21lama <lamasabaneh671@gmail.com>
2025-06-16 16:52:49 +0300

    add new js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@
  const deleteDoneBtn = document.querySelector('.delete-done');
  const deleteAllBtn = document.querySelector('.delete-all');
  const addBtn = document.getElementById('addBtn');
+ let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let currentFilter = 'all';
+
+const saveTasks = () => localStorage.setItem('tasks', JSON.stringify(tasks));
